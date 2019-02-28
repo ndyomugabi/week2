3e44d96a77f742f7f2c610d17974f6fbe83c0d97
ndyomugabi <ester2107@gmail.com>
2019-02-28 20:06:33 +0300

    fizzbuzz commit

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,14 +1,16 @@
-list1 = [int(x) for x in input("enter your 1st list of numbers with spaces: ").split()]
-list2 = [int(x) for x in input("enter your 2nd list of numbers with spaces: ").split()]
-def fizzbuzz(list1, list2):
+# list1 = [int(x) for x in input("enter your 1st list of numbers with spaces: ").split()]
+# list2 = [int(x) for x in input("enter your 2nd list of numbers with spaces: ").split()]
+def fizz_buzz(list1, list2):
     total_length = len(list1) + len(list2)
     if total_length% 3==0:
         print("fizz")
+        return "fizz"
     elif total_length  % 5 ==0:
         print("buzz")
+        return "buzz"
     else:
         print(total_length)
+        return total_length
 
-fizzbuzz(list1,list2)
 
 
